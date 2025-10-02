9fa5d73ac9a57a300518594aca811d64860b125f
ealrehaili <ealrehaili@ntgclarity.com>
2025-10-02 09:22:58 +0300

    map

--- composeApp/src/androidMain/kotlin/org/example/project/map/components/MapComponent.kt
+++ composeApp/src/androidMain/kotlin/org/example/project/map/components/MapComponent.kt
@@ -9,7 +9,6 @@ import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.MapProperties
-import com.google.maps.android.compose.MapUiSettings
 import com.google.maps.android.compose.Marker
 import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
