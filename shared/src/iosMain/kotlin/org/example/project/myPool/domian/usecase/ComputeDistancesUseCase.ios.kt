cb5670242e5d249a2c4fea70c54df31cee757484
Renad0515 <rsalah@ntgclarity.com>
2025-10-06 16:38:00 +0300

    decoupling the viewModels

--- shared/src/iosMain/kotlin/org/example/project/myPool/domian/usecase/ComputeDistancesUseCase.ios.kt
+++ shared/src/iosMain/kotlin/org/example/project/myPool/domian/usecase/ComputeDistancesUseCase.ios.kt
@@ -1,6 +1,7 @@
 package org.example.project.myPool.domian.usecase
 
 import org.example.project.myPool.domian.model.OrderInfo
+import org.example.project.socket.Coordinates
 
 actual class ComputeDistancesUseCase actual constructor() {
     actual operator fun invoke(
